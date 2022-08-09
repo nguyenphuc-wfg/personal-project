f5dafceb82ab4e0b90b6a8ac774495e944329acf
phucnhk <phucnhk@wolffungame.com>
2022-08-09 19:05:24 +0700

    udpate

--- Assets/Scripts/Tank/TankEffect.cs
+++ Assets/Scripts/Tank/TankEffect.cs
@@ -1,5 +1,14 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class TankEffect : MonoBehaviour {
-    
+    private List<Effect> listEffect = new List<Effect>();
+
+    public void AddEffect(Effect effect){
+
+    }
+    public void RemoveEffect(Effect effect){
+
+    }
 }
